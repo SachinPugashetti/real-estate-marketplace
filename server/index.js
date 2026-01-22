d38c2214c8e21edc635478a3acbcb2d46e9926a0
SachinPugashetti <pugashettisachin@gmail.com>
2026-01-22 12:50:02 +0530

    create sign up api route

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -10,13 +11,17 @@ mongoose
     console.log("Connected to MongoDB!");
   })
   .catch((err) => {
-    console.log(err);
+    console.log(err.message);
   });
 
 const app = express();
+
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
 
+console.log(process.env.MONGO);
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
